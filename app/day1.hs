562672d54c8ac4409f81cb4f88c4423168258fcf
Burak Güner <burak.guner@callstack.com>
2023-12-01 09:22:20 +0300

    feat: solve day1 first half

--- app/day1.hs
+++ app/day1.hs
@@ -1,10 +1,25 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+import Data.Char (isDigit)
 import Data.FileEmbed (embedStringFile)
+import Data.Maybe (catMaybes, isJust)
+
+writtenDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 input :: String
 input = $(embedStringFile "input/day1.txt")
 
+mapWithIndex :: ((a, Int) -> b) -> [a] -> [b]
+mapWithIndex mapper list = zipWith (curry mapper) list [0 ..]
+
+filterFirstAndLastDigits :: String -> String
+filterFirstAndLastDigits line =
+  let filteredIndices = catMaybes $ mapWithIndex (\(item, index) -> if isDigit item then Just index else Nothing) line
+   in [line !! (head filteredIndices), line !! (last filteredIndices)]
+
+readToInt :: String -> Integer
+readToInt = read
+
 main :: IO ()
 main = do
-  print "Hi mom"
+  print . sum . map (readToInt . filterFirstAndLastDigits) $ lines input
