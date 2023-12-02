4ddc5f38277c934162f3495979de690216ce6947
Burak Güner <burak.guner@callstack.com>
2023-12-02 18:03:10 +0300

    test: add unit tests

--- app/day0.hs
+++ app/day0.hs
@@ -1,10 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Data.FileEmbed (embedStringFile)
+import Test.HUnit
 
 input :: String
 input = $(embedStringFile "input/day0.txt")
 
+testHello = TestCase $ do
+  assertEqual "can say hello to John Doe" (hello "John Doe") "Hello John Doe"
+
+hello :: String -> String
+hello name = "Hello " ++ name
+
+main' = do
+  print $ hello "mom"
+
 main :: IO ()
 main = do
-  print "Hi mom"
+  result <- runTestTT $ TestList [testHello]
+  main'
--- app/day2.hs
+++ app/day2.hs
@@ -3,20 +3,21 @@
 
 import Data.FileEmbed (embedStringFile)
 import Data.List.Split (splitOn)
+import Test.HUnit
 import Prelude hiding (id)
 
 data Round = Round
   { id :: Int,
     cubeSets :: [CubeSet]
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 data CubeSet = Cubeset
   { red :: Int,
     green :: Int,
     blue :: Int
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 zeroCubes = Cubeset {red = 0, green = 0, blue = 0}
 
@@ -43,6 +44,11 @@ sumCubesets :: String -> CubeSet
 sumCubesets input =
   let cubes = map parseCubeSet $ splitOn ", " input
    in foldr add zeroCubes cubes
+
+testParseCubeSet =
+  TestCase $ do
+    assertEqual "parses red cubeset" (parseCubeSet "1 red") Cubeset {red = 1, green = 0, blue = 0}
+    assertEqual "parses multiple cubes in cubeset" (parseCubeSet "1 red, 888 green, 0 blue") Cubeset {red = 1, green = 888, blue = 0}
 
 parseCubeSet :: String -> CubeSet
 parseCubeSet input =
@@ -77,7 +83,12 @@ power Cubeset {red, green, blue} = red * green * blue
 input :: String
 input = $(embedStringFile "input/day2.txt")
 
-main :: IO ()
-main = do
+defaultMain :: IO ()
+defaultMain = do
   putStrLn $ "Part1: " ++ show (foldr (\round b -> b + id round) 0 . filter (all haveEnoughCubes . cubeSets) . map parseRound $ lines input)
   putStrLn $ "Part2: " ++ show (foldr (\round b -> b + (power . findMaxCubeSet $ cubeSets round)) 0 . map parseRound $ lines input)
+
+main :: IO ()
+main = do
+  result <- runTestTT $ TestList [testParseCubeSet]
+  defaultMain
