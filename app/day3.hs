d354233a56b0178ca414f1b5f723fea09b4adeeb
Burak Güner <burak.guner@callstack.com>
2023-12-03 21:20:55 +0300

    feat: solve second half

--- app/day3.hs
+++ app/day3.hs
@@ -2,6 +2,10 @@
 
 import Data.Char
 import Data.FileEmbed (embedStringFile)
+import Data.List
+import Data.List.GroupBy ()
+import Data.Maybe
+import Debug.Trace
 import Test.HUnit
 
 input :: String
@@ -34,6 +38,9 @@ testGetNeighboringCells = TestCase $ do
 getNeighboringCells :: Pos -> [Pos]
 getNeighboringCells pos = (\p -> (fst p + fst pos, snd p + snd pos)) <$> neighbourMap
 
+getValueToSort :: Pos -> Int
+getValueToSort pos = fst pos * 10 + snd pos
+
 testGetNextCell = TestCase $ do
   assertEqual "get next x" (1, 0) (getNextCell ["...", "..."] (0, 0))
   assertEqual "get next y" (0, 1) (getNextCell ["...", "..."] (2, 0))
@@ -43,40 +50,55 @@ getNextCell maze pos
   | fst pos < length (head maze) - 1 = (fst pos + 1, snd pos)
   | otherwise = (0, snd pos + 1)
 
-testIsIslandValid = TestCase $ do
-  assertEqual "Can detect valid island" True (isIslandValid ["123..", ".*..."] [] (0, 0))
-  assertEqual "Can detect invalid island" False (isIslandValid ["123..", "....."] [] (0, 0))
-  assertEqual "Can detect invalid iland with far symbol" False (isIslandValid ["123..", "....*"] [] (0, 0))
-  assertEqual "Can detect diagonal symbol" True (isIslandValid ["123..", "...*."] [] (0, 0))
+testGetPreviousCell = TestCase $ do
+  assertEqual "get previous x" (0, 0) (getPreviousCell ["...", "..."] (1, 0))
+  assertEqual "get previous y" (2, 0) (getPreviousCell ["...", "..."] (0, 1))
 
-isIslandValid :: Maze -> Visited -> Pos -> Bool
+getPreviousCell :: Maze -> Pos -> Pos
+getPreviousCell maze pos
+  | fst pos > 0 = (fst pos - 1, snd pos)
+  | otherwise = (length (head maze) - 1, snd pos - 1)
+
+isIslandValid :: Maze -> Visited -> Pos -> Maybe Pos
 isIslandValid maze visited pos =
   case getCell pos maze of
-    Just currentCell | pos `elem` visited -> False
-    Just currentCell | isDigit currentCell -> any (isIslandValid maze (pos : visited)) (getNeighboringCells pos)
-    Just '.' -> False
-    Just currentCell -> True -- Symbol, means current island is valid
-    Nothing -> False
+    Just currentCell | pos `elem` visited -> Nothing
+    Just currentCell
+      | isDigit currentCell ->
+          let positions = map ((isIslandValid maze (pos : visited))) (getNeighboringCells pos)
+              result = filter (isJust) positions
+           in if not (null result) then head result else Nothing
+    Just '*' -> Just pos
+    _ -> Nothing
 
 testJoinDecimal = TestCase $ do
-  assertEqual "Basic decimal join" 7000 (joinDecimal [7, 0, 0, 0])
+  assertEqual "Basic decimal jon" 7000 (joinDecimal [7, 0, 0, 0])
   assertEqual "Basic decimal join" 492 (joinDecimal [4, 9, 2])
 
 joinDecimal :: [Int] -> Int
 joinDecimal input = foldr (\(digit, power) acc -> acc + (digit * (10 ^ power))) 0 $ zip (reverse input) [0 ..]
 
-searchValidIslands :: Maze -> Pos -> [Int] -> [Int] -> [Int]
+searchValidIslands :: Maze -> Pos -> [Int] -> [(Int, Pos)] -> [(Int, Pos)]
 searchValidIslands maze pos currentStack resultStack =
   case getCell pos maze of
-    Just currentCell | isDigit currentCell && (fst pos == length (head maze) - 1) -> searchValidIslands maze (getNextCell maze pos) [] ((if isIslandValid maze [] pos then joinDecimal (currentStack ++ [digitToInt currentCell]) else 0) : resultStack)
+    Just currentCell
+      | isDigit currentCell && (fst pos == length (head maze) - 1) ->
+          let isValid = isIslandValid maze [] (getPreviousCell maze pos)
+           in case isValid of
+                Just asterixPos -> searchValidIslands maze (getNextCell maze pos) [] ((joinDecimal (currentStack ++ [digitToInt currentCell]), asterixPos) : resultStack)
+                Nothing -> searchValidIslands maze (getNextCell maze pos) [] resultStack
     Just currentCell | isDigit currentCell -> searchValidIslands maze (getNextCell maze pos) (currentStack ++ [digitToInt currentCell]) resultStack
-    Just currentCell -> searchValidIslands maze (getNextCell maze pos) [] ((if isIslandValid maze [] (fst pos - 1, snd pos) then joinDecimal currentStack else 0) : resultStack) -- Symbol
-    Nothing -> (if not (null currentStack) then joinDecimal currentStack else 0) : resultStack -- Traveled the whole list
+    Just currentCell ->
+      let isValid = isIslandValid maze [] (getPreviousCell maze pos)
+       in case isValid of
+            Just asterixPos -> searchValidIslands maze (getNextCell maze pos) [] ((joinDecimal currentStack, asterixPos) : resultStack) -- Symbol
+            Nothing -> searchValidIslands maze (getNextCell maze pos) [] resultStack
+    Nothing -> resultStack -- Traveled the whole list
 
 main' = do
-  print . sum . filter (/= 0) $ searchValidIslands (lines input) (0, 0) [] []
+  print . sum . map (\[a, b] -> (fst a) * (fst b)) $ filter (\islandGroup -> length islandGroup > 1) $ groupBy (\(_, posA) (_, posB) -> (fst posA == fst posB) && (snd posA == snd posB)) $ filter (\pos -> fst pos /= 0) $ sortBy (\(_, posA) (_, posB) -> compare (getValueToSort posA) (getValueToSort posB)) $ searchValidIslands (lines input) (0, 0) [] []
 
 main :: IO ()
 main = do
-  result <- runTestTT $ TestList [testGetCell, testGetNeighboringCells, testIsIslandValid, testGetNextCell, testJoinDecimal]
+  result <- runTestTT $ TestList [testGetCell, testGetNeighboringCells, testGetNextCell, testJoinDecimal, testGetPreviousCell]
   main'
